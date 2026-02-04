3b34b465724f9828483143f3e6d6fbc9d4f9f2db
djunekz <gab288.gab288@passinbox.com>
2026-02-05 06:48:33 +0700

    Update core/unlock

--- core/unlock.py
+++ core/unlock.py
@@ -2,10 +2,9 @@ from modules.miunlock_wrapper import run_miunlock
 from core.logger import log
 
 def unlock_bootloader():
-    """Wrapper for official miunlock binary"""
     log("Starting bootloader unlock...")
     success = run_miunlock()
     if success:
-        log("Bootloader unlocked successfully")
+        log("Bootloader unlocked successfully", level="success")
     else:
         log("Bootloader unlock failed", level="error")
