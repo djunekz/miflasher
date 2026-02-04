d3e3c359a00769f3dded9db4a1f8b8802f25133a
djunekz <gab288.gab288@passinbox.com>
2026-02-05 06:43:09 +0700

    Added core/logger.py

---
+++ core/logger.py
@@ -0,0 +1,10 @@
+import sys
+
+def log(message, level="info"):
+    colors = {
+        "info": "\033[1;34m",
+        "success": "\033[1;32m",
+        "error": "\033[1;31m"
+    }
+    reset = "\033[0m"
+    print(f"{colors.get(level, '')}[{level.upper()}]{reset} {message}")
