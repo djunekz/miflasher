4844347c0b904f0fbd5f12844b14b8376ea1ef86
djunekz <gab288.gab288@passinbox.com>
2026-02-05 08:07:58 +0700

    Update modules

--- modules/miunlock_wrapper.py
+++ modules/miunlock_wrapper.py
@@ -2,20 +2,13 @@ import subprocess
 from core.logger import log
 
 def run_miunlock():
-    """
-    Jalankan MiUnlock langsung di terminal.
-    Tidak menggunakan capture_output agar input user diterima.
-    Tetap menangani 401 jika muncul.
-    """
     log("Running MiUnlock tool...")
 
     try:
-        process = subprocess.Popen(
-            ["miunlock"],
-        )
+        process = subprocess.Popen(["miunlock"])
         process.wait()
+
         if process.returncode == 0:
-            log("Bootloader unlocked!", level="success")
             return True
         else:
             log(f"MiUnlock exited with code {process.returncode}", level="error")
