da52fc28afef8959451dc634a05c59b958bb28ec
djunekz <gab288.gab288@passinbox.com>
2026-02-05 07:10:33 +0700

    Update fixed

--- core/downloader.py
+++ core/downloader.py
@@ -1,4 +1,4 @@
-from core.utils import run_cmd
+from core.utils import run_cmd, progress_bar
 from core.logger import log
 import requests
 import os
@@ -10,9 +10,14 @@ def download_rom(url, dest=None):
     filename = os.path.join(dest, url.split("/")[-1])
     log(f"Downloading ROM: {filename}")
     r = requests.get(url, stream=True)
-    with open(filename, "wb") as f:
-        for chunk in r.iter_content(chunk_size=8192):
-            if chunk:
-                f.write(chunk)
+    total_length = r.headers.get('content-length')
+
+    with open(filename, 'wb') as f:
+        if total_length is None:
+            f.write(r.content)
+        else:
+            for chunk in progress_bar(r.iter_content(chunk_size=8192), prefix="Downloading:"):
+                if chunk:
+                    f.write(chunk)
     log(f"Download completed: {filename}", level="success")
     return filename
