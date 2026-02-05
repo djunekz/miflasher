d4aa66ccd586033600a40742f52cb3aae888e160
djunekz <gab288.gab288@passinbox.com>
2026-02-05 07:05:47 +0700

    Update error fixed

--- core/logger.py
+++ core/logger.py
@@ -4,7 +4,8 @@ def log(message, level="info"):
     colors = {
         "info": "\033[1;34m",
         "success": "\033[1;32m",
-        "error": "\033[1;31m"
+        "error": "\033[1;31m",
+        "warning": "\033[1;33m"
     }
     reset = "\033[0m"
     print(f"{colors.get(level, '')}[{level.upper()}]{reset} {message}")
