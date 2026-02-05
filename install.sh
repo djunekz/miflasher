91b1763e8e88383edf108e5010819e77c5de684f
djunekz <gab288.gab288@passinbox.com>
2026-02-05 07:24:57 +0700

    Update installer

--- install.sh
+++ install.sh
@@ -3,15 +3,11 @@ set -e
 echo "[..] Updating packages..."
 pkg update -y && pkg upgrade -y
 
-echo "[..] Installing Python3..."
-pkg install python3 -y
+echo "[..] Installing Python3 and dependencies..."
+pkg install python3 pv libusb -y
 pip install --upgrade pip
-
-echo "[..] Installing dependencies..."
-pkg install pv libusb -y
 pip install -r requirements.txt
 
-echo "[..] Making CLI executable..."
 chmod +x miflasher
 ln -sf $(pwd)/miflasher $PREFIX/bin/miflasher
 
